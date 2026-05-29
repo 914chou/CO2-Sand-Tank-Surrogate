5e8e918f8b374396ab903911bcd36510fc1f69cd
914chou <914chou@github.com>
2026-05-29 09:29:04 -0500

    add testing loss concat

--- post_processing/history_appending.py
+++ post_processing/history_appending.py
@@ -39,6 +39,7 @@ plotting_folder.mkdir(parents=True, exist_ok=True)
 # Concatentate the losses from all runs together so we can plot results
 train_loss_list = []
 val_loss_list = []
+test_loss_list = []
 epoch_list = []
 
 for i in range(final_loop_num):
@@ -46,17 +47,22 @@ for i in range(final_loop_num):
     history_info = loadmat(filename)
     train_loss = np.array(history_info['train_loss']) 
     val_loss = np.array(history_info['val_loss'])
+    test_loss = np.array(history_info['test_loss'])
+
     train_loss_list.append(train_loss)
     val_loss_list.append(val_loss)
+    test_loss_list.append(test_loss)
 
     # Also obtain the info of the number of actual epochs ran per loop
     epoch_list.append(history_info['epochs_ran'].item())
    
 combined_train_loss = np.concatenate(train_loss_list, axis = 1)
 combined_val_loss = np.concatenate(val_loss_list, axis = 1)
+combined_test_loss = np.concatenate(test_loss_list, axis = 1)
 
 final_train_loss = train_loss[:,-1] 
 final_val_loss = val_loss[:,-1]
+final_test_loss = test_loss[:,-1]
 total_epochs = sum(epoch_list)
 
 print("\n")
@@ -64,8 +70,10 @@ print("=" * 40)
 print("\n")
 print(f"Final training loss: {final_train_loss.item():.3e}")
 print(f"Final validation loss: {final_val_loss.item():.3e}")
+print(f"Final test loss: {final_test_loss.item():.3e}")
 print(f"Total epochs for the training: {total_epochs}")
 print("\n")
 
 np.save(plotting_folder / 'train_loss.npy', combined_train_loss)
 np.save(plotting_folder / 'val_loss.npy', combined_val_loss)
+np.save(plotting_folder / 'test_loss.npy', combined_test_loss)
